633206c857bc6e27680d7058f8f99c95eff782f9
ridid44 <ridid44@DESKTOP-CJHMFEQ>
2019-09-01 17:57:31 -0600

    Added methods to find and highlight keyword in Tweet text. Added functionality for quoted tweets

--- public/js/Tweet.js
+++ public/js/Tweet.js
@@ -1,22 +1,34 @@
 class Tweet {
     tweet;
-    extendedTweet;
 
     constructor(raw, keyword) {
         try {
             this.tweet = JSON.parse(raw);
+            // IF TOP LEVEL TWEET IS EXTENDED
             if (typeof this.tweet.extended_tweet !== 'undefined') {
                 this.SetExtendedTweet();
             }
+            if (typeof this.tweet.quoted_status !== 'undefined'  && this.tweet.retweeted_status == null) {
+                console.log('most');
+                this.tweet.retweeted_status = this.tweet.quoted_status;
+            } else {
+                this.tweet.quoted_status = null;
+            }
             if (typeof this.tweet.retweeted_status !== 'undefined'){
+                // IF RETWEETED TEXT IS EXTENDED
+                if (typeof this.tweet.retweeted_status.extended_tweet !== 'undefined'){
+                    this.SetExtendedRetweet();
+                }
                 this.FormatDate(true);
                 this.FormatRetweetDate();
                 this.SetRetweetProfileURL();
+                this.LocateKeywordRetweet(keyword);
             } else {
                 this.FormatDate(false);
                 this.tweet.retweeted_status = null;
             }
             this.SetProfileURL();
+            this.LocateKeyword(keyword);
         } catch(err) {
             console.log('Parse Error -- Skipping');
             this.tweet = null;
@@ -24,8 +36,11 @@ class Tweet {
     }
 
     SetExtendedTweet() {
-        this.extendedTweet = true;
         this.tweet.text = this.tweet.extended_tweet.full_text;
+    }
+
+    SetExtendedRetweet() {
+        this.tweet.retweeted_status.text = this.tweet.retweeted_status.extended_tweet.full_text;
     }
 
     SetProfileURL() {
@@ -50,5 +65,19 @@ class Tweet {
 
     SetRetweetProfileURL() {
         this.tweet.retweeted_status.user.profile_url = 'https://twitter.com/' + this.tweet.retweeted_status.user.screen_name;
+    }
+
+    LocateKeywordRetweet(word) {
+        var regexp = new RegExp(word, "gi");
+        let string = this.tweet.retweeted_status.text;
+        let replace = '<b class="keyword">' + word + '</b>';
+        this.tweet.retweeted_status.text = string.replace(regexp, replace);
+    }
+
+    LocateKeyword(word) {
+        var regexp = new RegExp(word, "gi");
+        let string = this.tweet.text;
+        let replace = '<b class="keyword">' + word + '</b>';
+        this.tweet.text = string.replace(regexp, replace);
     }
 }
